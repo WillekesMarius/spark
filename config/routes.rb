b7e9f200d9396bc9f17ab7a369fc0ea31d28905f
sophiabanta <skbanta123@gmail.com>
2023-08-29 17:05:02 +0200

    created and styled profile page. still needs work

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-  devise_for :users
-  root to: "pages#home"
-=======
   root to: "pages#home"
 
   devise_for :users
-  
-
-  resources :
-  resources :
-  resources :
-
-
-
-  get 'categories/index'
-  get 'categories/show'
-  get 'reviews/show'
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'favorites/create'
-  get 'favorites/edit'
-  get 'favorites/update'
-  get 'favorites/destroy'
-  get 'suggestions/index'
-  get 'suggestions/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
->>>>>>> Stashed changes
 
   resources :suggestions, only: %i[index show]
   resources :favorites, only: %i[create edit update destroy]
