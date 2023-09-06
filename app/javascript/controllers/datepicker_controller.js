ba26449bb7a86fa50313e3e00e6cbf47a664f6ab
prachijain <prachijain.work@gmail.com>
2023-09-07 00:39:27 +0200

    Fixed the slider, show page added new images

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -11,3 +11,18 @@ export default class extends Controller {
     });
   }
 }
+
+
+
+  // 1. name the funnction eg. 'confirmdDate'
+  // 2. go to html file && 'data: {action: 'click->datepicker#confirmdDate'}
+
+  // need to connect stimulus
+
+  const button = document.querySelector("#create-spark")
+  button.addEventListener("click", (event) => {
+    console.log(event);
+    event.currentTarget.classList.remove("btn-primary");
+    event.currentTarget.classList.add("btn-danger");
+    event.currentTarget.innerText = "You have successfully created this event!";
+  })
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   has_many :favorites
   has_many :suggestions, through: :favorites
+  has_one_attached :photo
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
